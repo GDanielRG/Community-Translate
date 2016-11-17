f527ba2e44fb44bc3a636fcfdd04f6c4f953d653
Gerardo Daniel Rodríguez <danyrod94@gmail.com>
2016-11-17 15:20:31 -0800

    States and keys

--- database/migrations/2013_10_11_225755_create_states_table.php
+++ database/migrations/2013_10_11_225755_create_states_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateStatesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('states', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('state_id')->unsigned();
-            $table->string('username');
-            $table->string('password');
-            $table->string('lastActivePlatform');
+            $table->string('name');
             $table->timestamps();
-
-            $table->foreign('state_id')->references('id')->on('states');
         });
     }
 
@@ -32,6 +27,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('states');
     }
 }
