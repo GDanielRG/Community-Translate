82867a960696940b708bb3d9f46dc5ce754d11d7
Gerardo Daniel Rodríguez <danyrod94@gmail.com>
2016-11-18 04:03:30 -0800

    test

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\GlobalFunctions;
 use App\Http\MainFunctions;
+use App\Message;
+use App\User;
+use App\Key;
 
 class HomeController extends Controller
 {
@@ -24,6 +27,23 @@ class HomeController extends Controller
      */
     public function index()
     {
+        $user =  User::find(1);
+
+        // Register the new user key
+        $user->keys()->save(new Key([	'name' => 'slack',
+                                                    'key' => 'D34AY5DT6']));
+
+        $message = new Message(['message' => trans('messages.new_user_registered')]);
+        $user->messages()->save($message);
+        $message = new Message(['message' => '1']);
+        $user->messages()->save($message);
+        $message = new Message(['message' => '2']);
+        $user->messages()->save($message);
+        $message = new Message(['message' => '3']);
+        $user->messages()->save($message);
+
+        $messages = Message::where('sent', false)->get();
+
         // Test register
         // $mainFunctions = new MainFunctions("facebook", "44", "alfhh hinojosa");
         // $mainFunctions->register();
@@ -43,8 +63,8 @@ class HomeController extends Controller
 
         // echo("<p style='white-space: pre-wrap;'>Comandos disponibles:\n\n#mute\nCambia tu estado actual a silenciado\n\n#unmute\nCambia tu estado actual a no silenciado\n\n#addLanguage {name|code}\nAgrega un nuevo lenguage a tu lista de lenguages conocidos\n\t-name: Nombre del lenguage\n\t-code: ISO: 693-2 codigo del lenguage\n\n#register {username} {password}\nRegistra una nueva aplicacion a tu cuenta\n\t-username: Tu ID\n\t-password: Tu contraseña\n\n#changeLanguage {name|code}\nCambia el lenguaje de la aplicacion\n\t-name: Nombre del lenguage\n\t-code: ISO: 693-2 codigo del lenguage\n</p>");
 
-        $mainFunctions = new MainFunctions("facebook", "44", "");
-        $mainFunctions->askHelp();
+        // $mainFunctions = new MainFunctions("facebook", "44", "");
+        // $mainFunctions->askHelp();
 
         return 'done';
     }
@@ -56,6 +76,13 @@ class HomeController extends Controller
 
     public function receiveText(Request $request)
     {
-        \Log::info($request->all());
+        $text =$request->get('message')["data"]["text"];
+        $id = $request->get('message')["data"]["id"]);
+        $service = $request->get('message')["service"]);
+        if(substr($text, 0, strlen('#register')) === "#register" && $text['9'] = " ")
+        {
+            $mainFunctions = new MainFunctions($service, $id, substr($text, 10));
+            $mainFunctions->register();
+        }
     }
 }
