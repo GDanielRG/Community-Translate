7ee63995a01b54189947484283968edbd87ffc14
Gerardo Daniel Rodríguez <danyrod94@gmail.com>
2016-11-18 10:45:52 -0800

    lelelele

--- app/Http/MainFunctions.php
+++ app/Http/MainFunctions.php
@@ -88,7 +88,7 @@
 				if($petition->translationRequest->user->canReceiveAnswers())
 				{
 					$message = new Message(['message' => trans('reply_label', ["language" => $petition->language->name, "translation" => $text])]);
-					$targetUserId = $petition->translationRequest->user();
+					$targetUserId = $petition->translationRequest->user;
 
 					$targetUserId->messages()->save($message);
 				}
